c1a230414fe2e28d16414ad23e77d575d778a0d1
殷天文 <yintianwen7@qq.com>
2018-09-18 23:22:45 +0800

    spring cloud Eureka 注册与发现

---
+++ spring-cloud-eureka/service-hi/src/main/java/com/gitee/taven/ServiceHiApplication.java
@@ -0,0 +1,12 @@
+package com.gitee.taven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHiApplication.class, args);
+	}
+}
