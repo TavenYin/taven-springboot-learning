14a7b55447c4edf84163078459cd3737c59ffbba
殷天文 <yintianwen7@qq.com>
2018-09-22 22:21:55 +0800

    spring cloud restTemplate ribbon

---
+++ spring-cloud-eureka/service-ribbon/src/main/java/com/gitee/taven/controller/HelloControler.java
@@ -0,0 +1,20 @@
+package com.gitee.taven.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gitee.taven.service.HelloService;
+
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService(name);
+    }
+}
---
+++ spring-cloud-eureka/service-ribbon/src/main/java/com/gitee/taven/service/HelloService.java
@@ -0,0 +1,17 @@
+package com.gitee.taven.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+}
