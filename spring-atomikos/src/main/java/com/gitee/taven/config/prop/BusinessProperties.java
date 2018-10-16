99444a8f6011b57ec605fa4d24b23ef05d13d929
殷天文 <yintianwen7@qq.com>
2018-10-16 14:05:42 +0800

    多数据源分布式事务

---
+++ spring-atomikos/src/main/java/com/gitee/taven/config/prop/BusinessProperties.java
@@ -0,0 +1,201 @@
+package com.gitee.taven.config.prop;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="spring.datasource.druid.business-db")
+public class BusinessProperties {
+
+	private String url;
+	
+	private String username;
+	
+	private String password;
+
+	private Integer initialSize;
+	
+	private Integer minIdle;
+	
+	private Integer maxActive;
+	
+	private Integer maxWait;
+	
+	private Integer timeBetweenEvictionRunsMillis;
+	
+	private Integer minEvictableIdleTimeMillis;
+	
+	private String validationQuery;
+	
+	private Integer validationQueryTimeout;
+	
+	private boolean testWhileIdle;
+	
+	private boolean testOnBorrow;
+	
+	private boolean testOnReturn;
+	
+	private boolean poolPreparedStatements;
+	
+	private Integer maxPoolPreparedStatementPerConnectionSize;
+	
+	private String connectionProperties;
+	
+	private boolean useGlobalDataSourceStat;
+	
+	private String filters;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getInitialSize() {
+		return initialSize;
+	}
+
+	public void setInitialSize(Integer initialSize) {
+		this.initialSize = initialSize;
+	}
+
+	public Integer getMinIdle() {
+		return minIdle;
+	}
+
+	public void setMinIdle(Integer minIdle) {
+		this.minIdle = minIdle;
+	}
+
+	public Integer getMaxActive() {
+		return maxActive;
+	}
+
+	public void setMaxActive(Integer maxActive) {
+		this.maxActive = maxActive;
+	}
+
+	public Integer getMaxWait() {
+		return maxWait;
+	}
+
+	public void setMaxWait(Integer maxWait) {
+		this.maxWait = maxWait;
+	}
+
+	public Integer getTimeBetweenEvictionRunsMillis() {
+		return timeBetweenEvictionRunsMillis;
+	}
+
+	public void setTimeBetweenEvictionRunsMillis(Integer timeBetweenEvictionRunsMillis) {
+		this.timeBetweenEvictionRunsMillis = timeBetweenEvictionRunsMillis;
+	}
+
+	public Integer getMinEvictableIdleTimeMillis() {
+		return minEvictableIdleTimeMillis;
+	}
+
+	public void setMinEvictableIdleTimeMillis(Integer minEvictableIdleTimeMillis) {
+		this.minEvictableIdleTimeMillis = minEvictableIdleTimeMillis;
+	}
+
+	public String getValidationQuery() {
+		return validationQuery;
+	}
+
+	public void setValidationQuery(String validationQuery) {
+		this.validationQuery = validationQuery;
+	}
+
+	public Integer getValidationQueryTimeout() {
+		return validationQueryTimeout;
+	}
+
+	public void setValidationQueryTimeout(Integer validationQueryTimeout) {
+		this.validationQueryTimeout = validationQueryTimeout;
+	}
+
+	public boolean isTestWhileIdle() {
+		return testWhileIdle;
+	}
+
+	public void setTestWhileIdle(boolean testWhileIdle) {
+		this.testWhileIdle = testWhileIdle;
+	}
+
+	public boolean isTestOnBorrow() {
+		return testOnBorrow;
+	}
+
+	public void setTestOnBorrow(boolean testOnBorrow) {
+		this.testOnBorrow = testOnBorrow;
+	}
+
+	public boolean isTestOnReturn() {
+		return testOnReturn;
+	}
+
+	public void setTestOnReturn(boolean testOnReturn) {
+		this.testOnReturn = testOnReturn;
+	}
+
+	public boolean isPoolPreparedStatements() {
+		return poolPreparedStatements;
+	}
+
+	public void setPoolPreparedStatements(boolean poolPreparedStatements) {
+		this.poolPreparedStatements = poolPreparedStatements;
+	}
+
+	public Integer getMaxPoolPreparedStatementPerConnectionSize() {
+		return maxPoolPreparedStatementPerConnectionSize;
+	}
+
+	public void setMaxPoolPreparedStatementPerConnectionSize(Integer maxPoolPreparedStatementPerConnectionSize) {
+		this.maxPoolPreparedStatementPerConnectionSize = maxPoolPreparedStatementPerConnectionSize;
+	}
+
+	public String getConnectionProperties() {
+		return connectionProperties;
+	}
+
+	public void setConnectionProperties(String connectionProperties) {
+		this.connectionProperties = connectionProperties;
+	}
+
+	public boolean isUseGlobalDataSourceStat() {
+		return useGlobalDataSourceStat;
+	}
+
+	public void setUseGlobalDataSourceStat(boolean useGlobalDataSourceStat) {
+		this.useGlobalDataSourceStat = useGlobalDataSourceStat;
+	}
+
+	public String getFilters() {
+		return filters;
+	}
+
+	public void setFilters(String filters) {
+		this.filters = filters;
+	}
+
+}
