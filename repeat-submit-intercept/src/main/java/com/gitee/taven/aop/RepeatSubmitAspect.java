0f56f1e23d00e548792d508a120e0bfb320a706d
殷天文 <yintianwen7@qq.com>
2019-04-03 16:51:51 +0800

    repeat-submit-intercept 保证请求一定被解锁

--- repeat-submit-intercept/src/main/java/com/gitee/taven/aop/RepeatSubmitAspect.java
+++ repeat-submit-intercept/src/main/java/com/gitee/taven/aop/RepeatSubmitAspect.java
@@ -3,7 +3,6 @@ package com.gitee.taven.aop;
 import com.gitee.taven.ApiResult;
 import com.gitee.taven.utils.RedisLock;
 import com.gitee.taven.utils.RequestUtils;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
@@ -29,42 +28,43 @@ public class RepeatSubmitAspect {
     }
 
     @Around("pointCut(noRepeatSubmit)")
-    public Object around(ProceedingJoinPoint pjp, NoRepeatSubmit noRepeatSubmit) {
+    public Object around(ProceedingJoinPoint pjp, NoRepeatSubmit noRepeatSubmit) throws Throwable {
         int lockSeconds = noRepeatSubmit.lockTime();
 
-        try {
-            HttpServletRequest request = RequestUtils.getRequest();
-            Assert.notNull(request, "request can not null");
+        HttpServletRequest request = RequestUtils.getRequest();
+        Assert.notNull(request, "request can not null");
 
-            // 此处可以用token或者JSessionId
-            String token = request.getHeader("Authorization");
-            String path = request.getServletPath();
-            String key = getKey(token, path);
-            String clientId = getClientId();
+        // 此处可以用token或者JSessionId
+        String token = request.getHeader("Authorization");
+        String path = request.getServletPath();
+        String key = getKey(token, path);
+        String clientId = getClientId();
 
-            boolean isSuccess = redisLock.tryLock(key, clientId, lockSeconds);
-            LOGGER.info("tryLock key = [{}], clientId = [{}]", key, clientId);
+        boolean isSuccess = redisLock.tryLock(key, clientId, lockSeconds);
+        LOGGER.info("tryLock key = [{}], clientId = [{}]", key, clientId);
 
-            if (isSuccess) {
-                LOGGER.info("tryLock success, key = [{}], clientId = [{}]", key, clientId);
-                // 获取锁成功, 执行进程
-                Object result = pjp.proceed();
+        if (isSuccess) {
+            LOGGER.info("tryLock success, key = [{}], clientId = [{}]", key, clientId);
+            // 获取锁成功
+            Object result;
+
+            try {
+                // 执行进程
+                result = pjp.proceed();
+            } finally {
                 // 解锁
                 redisLock.releaseLock(key, clientId);
                 LOGGER.info("releaseLock success, key = [{}], clientId = [{}]", key, clientId);
-                return result;
-
-            } else {
-                // 获取锁失败，认为是重复提交的请求
-                LOGGER.info("tryLock fail, key = [{}]", key);
-                return new ApiResult(200, "重复请求，请稍后再试", null);
             }
 
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
+            return result;
+
+        } else {
+            // 获取锁失败，认为是重复提交的请求
+            LOGGER.info("tryLock fail, key = [{}]", key);
+            return new ApiResult(200, "重复请求，请稍后再试", null);
         }
 
-        return new ApiResult(500, "系统异常", null);
     }
 
     private String getKey(String token, String path) {
