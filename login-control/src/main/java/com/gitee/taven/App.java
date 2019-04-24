5d3a0253c021ea6f82cbe028b2c785f55368e1a8
殷天文 <yintianwen7@qq.com>
2019-04-24 22:28:18 +0800

    queue filter 条件配置

--- login-control/src/main/java/com/gitee/taven/App.java
+++ login-control/src/main/java/com/gitee/taven/App.java
@@ -2,12 +2,16 @@ package com.gitee.taven;
 
 import com.gitee.taven.filter.CompareKickOutFilter;
 import com.gitee.taven.filter.KickOutFilter;
+import com.gitee.taven.filter.QueueKickOutFilter;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
@@ -24,14 +28,22 @@ public class App {
 		return Redisson.create(config);
 	}
 
+	@ConditionalOnProperty(value = {"queue-filter.enabled"})
 	@Bean
-	public KickOutFilter kickOutFilter() {
+	public KickOutFilter queueKickOutFilter() {
+		return new QueueKickOutFilter();
+	}
+
+	@ConditionalOnMissingBean(KickOutFilter.class)
+	@Bean
+	public KickOutFilter compareKickOutFilter() {
 		return new CompareKickOutFilter();
 	}
 
 	@Bean
-	public FilterRegistrationBean testFilterRegistration() {
-		FilterRegistrationBean registration = new FilterRegistrationBean(kickOutFilter());
+	public FilterRegistrationBean testFilterRegistration(KickOutFilter kickOutFilter) {
+		System.out.println(kickOutFilter);
+		FilterRegistrationBean registration = new FilterRegistrationBean(kickOutFilter);
 		registration.addUrlPatterns("/user/*");
 		registration.setName("kickOutFilter");
 		return registration;
