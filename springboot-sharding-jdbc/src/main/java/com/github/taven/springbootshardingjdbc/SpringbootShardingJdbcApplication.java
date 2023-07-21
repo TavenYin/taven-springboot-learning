cd832304ac47dadb06025ce146f0f82ad5aa40bf
si_tianwen.yin <si_tianwen.yin@zatech.com>
2023-07-21 17:58:35 +0800

    springboot-sharding-jdbc init

---
+++ springboot-sharding-jdbc/src/main/java/com/github/taven/springbootshardingjdbc/SpringbootShardingJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.github.taven.springbootshardingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShardingJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootShardingJdbcApplication.class, args);
+	}
+
+}
