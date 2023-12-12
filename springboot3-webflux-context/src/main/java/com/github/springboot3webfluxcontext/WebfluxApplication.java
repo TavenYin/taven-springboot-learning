3cce06c8322ad78c139a49edcfb1b64448f2ec11
TavenYin <yintianwen7@qq.com>
2023-12-12 22:30:28 +0800

    springboot3 webflux context demo

---
+++ springboot3-webflux-context/src/main/java/com/github/springboot3webfluxcontext/WebfluxApplication.java
@@ -0,0 +1,84 @@
+package com.github.springboot3webfluxcontext;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.function.Function;
+
+@Slf4j
+@RestController
+@SpringBootApplication
+public class WebfluxApplication {
+
+	static final ThreadLocal<String> contextThreadLocal = new ThreadLocal<>();
+
+	public static void main(String[] args) {
+//		Hooks.enableAutomaticContextPropagation();
+		Schedulers.onScheduleHook("THREAD_LOCAL_PROPAGATION", scheduleHook());
+		SpringApplication.run(WebfluxApplication.class, args);
+	}
+
+	public static Function<Runnable, Runnable> scheduleHook() {
+		return delegate -> {
+			// 执行这行的时候，还是在原来的线程
+			// 在原来的线程取值
+			String value = contextThreadLocal.get();
+			return () -> {
+				// set 到新线程的 threadLocal
+				contextThreadLocal.set(value);
+				delegate.run();
+			};
+		};
+	}
+
+	@Component
+	public static class ExampleWebFilter implements WebFilter {
+
+		@Override
+		public Mono<Void> filter(ServerWebExchange serverWebExchange,
+								 WebFilterChain webFilterChain) {
+			contextThreadLocal.set("hello world");
+			log.info("set value");
+			return webFilterChain.filter(serverWebExchange);
+		}
+	}
+
+	@GetMapping("test")
+	public Mono<String> test() {
+		return Mono.fromCallable(() -> {
+			log.info("contextThreadLocal: {}", contextThreadLocal.get());
+			return contextThreadLocal.get();
+		});
+	}
+
+	@GetMapping("subscribeOn")
+	public Mono<String> subscribeOn() {
+		return Mono.fromCallable(() -> {
+			log.info("contextThreadLocal: {}", contextThreadLocal.get());
+			return contextThreadLocal.get();
+		}).subscribeOn(Schedulers.boundedElastic());
+	}
+
+	@GetMapping("publishOn")
+	public Mono<String> publishOn() {
+		return Mono.fromCallable(() -> {
+			log.info("contextThreadLocal: {}", contextThreadLocal.get());
+			return contextThreadLocal.get();
+		}).subscribeOn(Schedulers.boundedElastic())
+				.publishOn(Schedulers.parallel())
+				.map(v -> {
+					log.info("contextThreadLocal: {}", contextThreadLocal.get());
+					return v;
+				});
+	}
+
+}
---
+++ springboot3-webflux-context/src/test/java/com/github/springboot3webfluxcontext/Springboot3WebfluxContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springboot3webfluxcontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3WebfluxContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
